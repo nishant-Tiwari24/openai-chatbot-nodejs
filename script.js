4bd8dc35d834d9bd130ade06634e000452d47475
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-11 23:58:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,21 @@ const generateResponse = (chatElement) => {
         .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 
+// let userMessage = null;
+// const inputInitHeight = chatInput.scrollHeight;
+
+// const rateLimitDelay = 1000;
+// let lastRequestTime = 0;
+
+// const createChatLi = (message, className) => {
+//     const chatLi = document.createElement("li");
+//     chatLi.classList.add("chat", `${className}`);
+//     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+//     chatLi.innerHTML = chatContent;
+//     chatLi.querySelector("p").textContent = message;
+//     return chatLi;
+// }
+
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     if (!userMessage) return;
